786a769a1916686e71a0f96f327f2f543e41cd1f
Hamit YILMAZ <hamityilmaz@outlook.com>
2019-12-20 17:30:25 +0300

    Add muuri dependency

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,60 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, AfterViewInit } from '@angular/core';
+
+declare let Muuri: any;
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
   title = 'ng-muuri';
+  elements = [...Array(10).keys()];
+
+  ngAfterViewInit(): void {
+    var grid = new Muuri('.grid', {
+      // Layout
+      layout: {
+        fillGaps: false,
+        horizontal: false,
+        alignRight: true,
+        alignBottom: true,
+        rounding: true
+      },
+
+      // Drag & Drop
+      dragEnabled: true,
+      dragContainer: null,
+      dragStartPredicate: {
+        distance: 0,
+        delay: 0,
+        handle: false
+      },
+      dragAxis: null,
+      dragSort: true,
+      dragSortHeuristics: {
+        sortInterval: 100,
+        minDragDistance: 10,
+        minBounceBackAngle: 1
+      },
+      dragReleaseDuration: 300,
+      dragReleaseEasing: 'ease',
+      dragCssProps: {
+        touchAction: 'none',
+        userSelect: 'none',
+        userDrag: 'none',
+        tapHighlightColor: 'rgba(0, 0, 0, 0)',
+        touchCallout: 'none',
+        contentZooming: 'none'
+      },
+      dragPlaceholder: {
+        enabled: false,
+        duration: 300,
+        easing: 'ease',
+        createElement: null,
+        onCreate: null,
+        onRemove: null
+      },
+    });
+  }
 }
